3bfeaa2db520a13fe074438f5208d823d7b6b31c
JLY191 <91368928+JLY191@users.noreply.github.com>
2023-12-27 23:29:45 +0800

    feat: add cors middleware

--- app/SetRoutes.go
+++ app/SetRoutes.go
@@ -2,11 +2,12 @@ package app
 
 import (
 	"PM_backend/app/controller"
+	"PM_backend/app/middleware"
 )
 
 func addRoutes() {
-	G.GET("/ping", controller.PingHandler)
-	userGroup := G.Group("/user")
+	G.GET("/ping", middleware.Cors(), controller.PingHandler)
+	userGroup := G.Group("/user", middleware.Cors())
 	userGroup.POST("/register", controller.RegisterHandler)
 	userGroup.POST("/login", controller.LoginHandler)
 	userGroup.GET("/logout", controller.LogoutHandler)
--- app/middleware/Middleware.go
+++ app/middleware/Middleware.go
@@ -4,6 +4,7 @@ import (
 	"PM_backend/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
+	"net/http"
 )
 
 func Auth() gin.HandlerFunc {
@@ -15,6 +16,23 @@ func Auth() gin.HandlerFunc {
 		}
 		name := utils.ParseToken(cookie)
 		c.Set("username", name)
+		c.Next()
+	}
+}
+
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization") // 添加 Authorization 头
+
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusOK)
+			return
+		}
+
 		c.Next()
 	}
 }
